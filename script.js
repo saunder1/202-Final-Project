344a9a7d0fca3127a0a00e84a940f412bce10347
Saunder VanWoerden <saunder@Saunders-MacBook-Pro.local>
2023-05-25 13:22:30 -0700

    script didnt push?

--- script.js
+++ script.js
@@ -14,23 +14,3 @@ ScrollTrigger.create({
     pin: "#learn"
 })
 
-// gsap.registerPlugin(ScrollTrigger);
-
-// gsap.to("#watch-content", {
-//   y: "-100%",  // Moves the #watch-content span upwards
-//   ease: "none",
-//   scrollTrigger: {
-//     trigger: "#watch",
-//     start: "top top",
-//     end: "bottom bottom",
-//     scrub: true
-//   }
-// });
-
-// ScrollTrigger.create({
-//   trigger: "#watch",
-//   start: "top top",
-//   end: "bottom bottom",
-//   pin: "#over-content"
-// });
-
