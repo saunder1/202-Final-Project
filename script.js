6151b11b5d2664dad424c0e18f0d232020f2c3a8
Saunder <vanwoes2@wwu.edu>
2023-05-27 15:33:13 -0700

    layered ow2 promises section

--- script.js
+++ script.js
@@ -96,52 +96,122 @@ gsap.to("#ow2-announced", {
     scrollTrigger: {
         trigger: "#ow2-announcement",
         start: "top 50%",
-        end: "top top",
+        end: "top 20%",
         scrub: true
     }
 })
 
-gsap.fromTo('#pve', 
-    // from
-    { x: "+=150%" }, 
-    // to
-    { x: "0%", 
-    duration: 3,
-    ease: 'none',
-    scrollTrigger: {
-        trigger: "#pve",
-        start: "top 80%",
-        end: "top 20%",
-        scrub: true
-    } 
+ScrollTrigger.create({
+    trigger: "#ow2-announcement",
+    start: "top top",
+    endTrigger: ".new-stuff",
+    end: "bottom bottom",
+    pin: "#ow2-announcement"
+})
+
+ScrollTrigger.create({
+    trigger: ".pve",
+    start: "top 50%",
+    pin: true, 
+    pinSpacing: false 
 });
 
-gsap.fromTo('#missions', 
-    // from
-    { x: "-=150%" }, 
-    // to
-    { x: "0%", 
-    duration: 3,
-    ease: 'none',
-    scrollTrigger: {
-        trigger: "#pve",
-        start: "top 80%",
-        end: "top 20%",
-        scrub: true
-    } 
+ScrollTrigger.create({
+    trigger: ".missions",
+    start: "top 50%",
+    endTrigger: ".new-stuff",
+    end: "bottom 102%",
+    pin: true, 
+    pinSpacing: false 
 });
 
-gsap.fromTo('#new-stuff', 
-    // from
-    { x: "+=150%" }, 
-    // to
-    { x: "0%", 
-      duration: 3,
-      ease: 'none',
-      scrollTrigger: {
-        trigger: "#pve",
-        start: "top 80%",
-        end: "top 20%",
-        scrub: true
-    } 
+ScrollTrigger.create({
+    trigger: ".new-stuff",
+    start: "top 50%",
+    end: "bottom bottom",
+    pin: true, 
+    pinSpacing: false 
 });
+
+// ScrollTrigger.create({
+//     trigger: ".pve",
+//     start: "top 50%",
+//     endTrigger: ".new-stuff",
+//     end: "bottom bottom",
+//     pin: ".pve"
+// })
+
+// ScrollTrigger.create({
+//     trigger: ""
+// })
+
+
+// // we'll create a ScrollTrigger for each panel just to track when each panel's top hits the top of the viewport (we only need this for snapping)
+// let tops = panels.map(panel => ScrollTrigger.create({trigger: panel, start: "top top"}));
+
+let panels = gsap.utils.toArray(".promises");
+
+panels.forEach((panel, i) => {
+  
+});
+
+// ScrollTrigger.create({
+//     scrub: true,
+//   snap: {
+//     snapTo: (progress, self) => {
+//       let panelStarts = tops.map(st => st.start), // an Array of all the starting scroll positions. We do this on each scroll to make sure it's totally responsive. Starting positions may change when the user resizes the viewport
+//           snapScroll = gsap.utils.snap(panelStarts, self.scroll()); // find the closest one
+//       return gsap.utils.normalize(0, ScrollTrigger.maxScroll(window), snapScroll); // snapping requires a progress value, so convert the scroll position into a normalized progress value between 0 and 1
+//     },
+//     duration: 0.5
+//   }
+// });
+
+
+
+// staggered <p> in from sides
+
+// gsap.fromTo('#pve', 
+//     // from
+//     { x: "+=150%" }, 
+//     // to
+//     { x: "0%", 
+//     duration: 3,
+//     ease: 'none',
+//     scrollTrigger: {
+//         trigger: "#pve",
+//         start: "top 80%",
+//         end: "top 20%",
+//         scrub: true
+//     } 
+// });
+
+// gsap.fromTo('#missions', 
+//     // from
+//     { x: "-=150%" }, 
+//     // to
+//     { x: "0%", 
+//     duration: 3,
+//     ease: 'none',
+//     scrollTrigger: {
+//         trigger: "#pve",
+//         start: "top 80%",
+//         end: "top 20%",
+//         scrub: true
+//     } 
+// });
+
+// gsap.fromTo('#new-stuff', 
+//     // from
+//     { x: "+=150%" }, 
+//     // to
+//     { x: "0%", 
+//       duration: 3,
+//       ease: 'none',
+//       scrollTrigger: {
+//         trigger: "#pve",
+//         start: "top 80%",
+//         end: "top 20%",
+//         scrub: true
+//     } 
+// });
